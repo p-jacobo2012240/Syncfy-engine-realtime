6ca7c513ea900c83d2cd6b65ff770182ff008f22
unknown <pablojacobo1998@gmail.com>
2018-11-24 23:08:18 -0600

    Sockets Config Inicial

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const skIO = require('socket.io')
+const http = require('http')
+
+let server = http.createServer(app)
 
 process.env.PORT = process.env.PORT || 3000
 
@@ -10,7 +14,15 @@ app.get('/', (req, res)=>{
     res.send({ message: 'Peticion Exitosa'})
 })
 
-app.listen(process.env.PORT, ()=>{
+let io = skIO(server)
+
+io.on('connection', (client)=>{
+    
+    console.log('Usuario Activo')
+
+})
+
+server.listen(process.env.PORT, ()=>{
     console.log(`
         Server Online -> ${process.env.PORT}
     `)
