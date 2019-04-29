fd385aa0620a25c3c9b6bff8199128eab13d67cf
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-04-28 23:47:30 -0600

    [Refactor] : model-server

--- global/environment.ts
+++
@@ -1,3 +0,0 @@
-
-
-export const SERVER_PORT: number = Number( process.env.PORT ) || 5000;
--- models/server.ts
+++ models/server.ts
@@ -1,62 +1,19 @@
+import express from 'express'
 
-import express from 'express';
-import { SERVER_PORT } from '../global/environment';
-import socketIO from 'socket.io';
-import http from 'http';
+export class Server{
 
-import * as socket from '../sockets/socket';
+    public app : express.Application
+    public port : Number = 3002 || process.env.PORT
+    public hck : any
 
-
-
-export default class Server {
-
-    private static _intance: Server;
-
-    public app: express.Application;
-    public port: number;
-
-    public io: socketIO.Server;
-    private httpServer: http.Server;
-
-
-    private constructor() {
-
-        this.app = express();
-        this.port = SERVER_PORT;
-
-        this.httpServer = new http.Server( this.app );
-        this.io = socketIO( this.httpServer );
-
-        this.escucharSockets();
+    constructor(){
+        this.app = express()
+        this.port
     }
 
-    public static get instance() {
-        return this._intance || ( this._intance = new this() );
-    }
+    async start( callback : any ){
 
-
-    private escucharSockets() {
-
-        console.log('Escuchando conexiones - sockets');
-
-        this.io.on('connection', cliente => {
-
-            console.log('Cliente conectado');
-
-            // Mensajes
-            socket.mensaje( cliente, this.io );
-
-            // Desconectar
-            socket.desconectar( cliente );         
-
-        });
-
-    }
-
-
-    start( callback: Function ) {
-
-        this.httpServer.listen( this.port, callback );
+        this.hck = await this.app.listen(  this.port, callback)
 
     }
 
