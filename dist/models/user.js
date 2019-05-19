c7f9458d60a252182a84e10de87665adecba1fcf
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-05-18 22:51:48 -0600

    model define users ib server

---
+++ dist/models/user.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(id_user, username, group) {
+        this.id_user = '';
+        this.username = 'no-name';
+        this.group = 'no-group';
+        this.id_user = id_user;
+        this.username = username;
+        this.group = group;
+    }
+}
+exports.User = User;
