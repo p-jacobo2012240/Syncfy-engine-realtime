849bf15f6749be9444556081221725c17d1d2ff7
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-05-29 22:51:18 -0600

    fixes

--- dist/models/server.js
+++ dist/models/server.js
@@ -51,13 +51,13 @@ class Server {
             //Disconnect
             broakerConfig.disconnect(client);
             /**
-             * @param HandlerCtrl(client)
+             * @param HandleCtrl(client)
              * @return observable SocketIo
              */
             broakerConfig.messages(client, this.io);
             /**
              *
-             * @param Handler Object (user)
+             * @param Handle Object (user)
              * @return void user
              */
             broakerConfig.loginMethod(client, this.io);
