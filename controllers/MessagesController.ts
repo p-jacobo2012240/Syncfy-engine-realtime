3a4838839db844cf2a9154a02753c04f20fcb4ab
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-06-02 23:03:40 -0600

    new Ctrl : using Singleton

---
+++ controllers/MessagesController.ts
@@ -0,0 +1,42 @@
+import { 
+    Request,
+    Response
+ } from 'express'
+
+
+export class MessagesCtrl {
+
+    private static _getInstance : MessagesCtrl
+
+    public static get getInstance(){
+        return this._getInstance || ( this._getInstance = new this ) 
+    } 
+
+    constructor(){}
+
+    private async sendOneToOne( req: Request, res: Response ) : Promise<any>{
+
+        let ctx = {
+            id : req.body.id,
+            data : req.body.data,
+            of: req.body.of
+        }
+
+        try {
+
+            let result = await res.status(200).json({
+                ok: true,
+                msg : 'sent..',
+                ctx
+            })
+            
+        } catch (e) {
+            res.json(`
+                An error has occurred : ${e}
+            `)
+        }
+
+    } 
+
+ }
+
