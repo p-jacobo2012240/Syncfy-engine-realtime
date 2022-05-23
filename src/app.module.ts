1d10c56ab0aed4ee7c1d89fbb93c0bb4afc5627d
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-05-22 22:10:45 -0600

    prepared hexagonal architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApplicationModule } from './engine/application/application.module';
+import { InfrastructureModule } from './engine/infrastructure/infrastructure.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ApplicationModule, 
+    InfrastructureModule
+  ],
+  providers: [],
 })
 export class AppModule {}
