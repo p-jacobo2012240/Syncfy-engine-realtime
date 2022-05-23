b5b0d947352fd035d02eaaca836575ff26111cc9
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-05-22 21:19:39 -0600

    this project it was migrated to other architecture for gcp apis interaction and socketio

--- dist/routes/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-let app = express_1.Router();
-let usersCtrl = userController_1.UsersCtrl.getInstance;
-app.get('/', usersCtrl.getUsers);
-app.get('/detail', usersCtrl.usersDetail);
-exports.default = app;
